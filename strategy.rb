9c69203e38cb8359ba96a7d65e8dd9a7107cb6f2
Roger Neel <pair+roger@mavenlink.com>
2013-09-20 17:55:52 -0700

    state is back

--- strategy.rb
+++ strategy.rb
@@ -3,26 +3,23 @@ Dir['./lib/*.rb'].each { |f| require f }
 include Aggressive
 include Defensive
 
-#@state = State.new
+@state = State.new
 
 on_turn do
-  #@state.save_battle(battle, me)
+  @state.save_battle(battle, me)
   turn = handle_turn
-  #@state.save_turn(turn)
+  @state.save_turn(turn)
   turn
 end
 
 def handle_turn
-  #if @state.just_hit_me?
-  #  act_defensively target_enemy
-  #elsif my.ammo >= 1 || (@state.enough_history && @state[-1].turn == 'defense' && @state[-2].turn == 'defense')
-  #  act_aggressively target_enemy
-  #else
-  #  act_defensively target_enemy
-  #end
-
-  act_aggressively target_enemy
-
+  if @state.just_hit_me?
+    act_defensively target_enemy
+  elsif my.ammo >= 1 || (@state.enough_history && @state[-1].turn == 'defense' && @state[-2].turn == 'defense')
+    act_aggressively target_enemy
+  else
+    act_defensively target_enemy
+  end
 end
 
 def target_enemy
