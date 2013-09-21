57c4cec601969ec5021f814bf1413fe4552d8147
Roger Neel <pair+roger@mavenlink.com>
2013-09-20 17:34:40 -0700

    stab

--- strategy.rb
+++ strategy.rb
@@ -13,9 +13,13 @@ on_turn do
 end
 
 def handle_turn
-  # return act_defensively target_enemy if @state.just_hit_me?
-  return act_aggressively target_enemy if my.ammo >= 1 || (@state.enough_history && @state[-1].turn == 'defense' && @state[-2].turn == 'defense')
-  act_defensively target_enemy
+  if @state.just_hit_me?
+    act_defensively target_enemy 
+  elsif my.ammo >= 1 || (@state.enough_history && @state[-1].turn == 'defense' && @state[-2].turn == 'defense')
+    act_aggressively target_enemy
+  else
+    act_defensively target_enemy
+  end
 end
 
 def target_enemy
