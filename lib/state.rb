3f7d053c85db2ab0ef58da9f9bd4ed34cd7e7019
Roger Neel <pair+roger@mavenlink.com>
2013-09-20 17:12:38 -0700

    better state saving and knowledge of hits...maybe

---
+++ lib/state.rb
@@ -0,0 +1,44 @@
+class State
+  def initialize
+    @history = []
+    @my_name = nil
+  end
+  
+  def save_battle(battle, me)
+    @history << BattleState.new battle
+    @my_name = me.username
+  end
+  
+  def save_turn(turn)
+    @history.last.turn = turn
+  end
+  
+  def enough_history
+    @history.length > 1
+  end
+
+  def me_now
+    historical_me @history[-1].battle
+  end
+
+  def me_previous
+    historical_me @history[-2].battle
+  end
+  
+  def historical_me(battle)
+    battle.robots.detect { |r| r.username == @my_name }
+  end
+  
+  def just_hit_me?
+    return false unless enough_history
+    me_previous.armor > me_now.armor
+  end
+end
+
+class BattleState
+  attr_accessor :battle, :turn
+  
+  def initialize(battle)
+    @battle = battle
+  end
+end
--- strategy.rb
+++ strategy.rb
@@ -3,18 +3,19 @@ Dir['./lib/*.rb'].each { |f| require f }
 include Aggressive
 include Defensive
 
-$history = []
+@state = State.new
 
 on_turn do
-  handle_turn
+  @state.save_battle(battle, me)
+  turn = handle_turn
+  @state.save_turn(turn)
+  turn
 end
 
 def handle_turn
-  if my.ammo >= 1 || ($history.length > 2 && $history[-1] == 'defense' && $history[-2] == 'defense')
-    act_aggressively target_enemy
-  else
-    act_defensively target_enemy
-  end
+  return act_defensively target_enemy if @state.just_hit_me?
+  return act_aggressively target_enemy if my.ammo >= 1 || (@state.enough_history && @state[-1].turn == 'defense' && @state[-2].turn == 'defense')
+  act_defensively target_enemy
 end
 
 def target_enemy
