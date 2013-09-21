c0de12b49240748d028c74448f3f50475de22ed2
Roger Neel <pair+roger@mavenlink.com>
2013-09-20 17:53:42 -0700

    argh! removing globals

--- lib/aggressive.rb
+++ lib/aggressive.rb
@@ -20,7 +20,6 @@ module Aggressive
   end
 
   def act_aggressively(enemy)
-    $history.push('offense')
     return hunt unless enemy
     return rest if my.ammo == 0
     return move_towards! enemy if obscured? enemy
